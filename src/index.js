e98342401e584a6b8dda643d5d2d8739de2146b8
Konrad <wudkowski@gmail.com>
2017-09-10 15:50:46 +0100

    basic jsx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 // <h1>Hello World</h1>
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React = require('react')
+var ReactDOM = require('react-dom')
 
-var App = React.createElement('h1', null, 'Hello World');
+// var App = React.createElement('h1', null, 'Hello World')
+
+// var App = (
+//     <ul>
+//         <li>Apple</li>
+//         <li>Orange</li>
+//         <li>Mango</li>
+//     </ul>
+// )
+
+var App = <h1>Hello World</h1>
+
+var Component = function() {
+    return (
+      <h1>Hello World</h1>  
+    )
+}
+
+// var Component = () => <h1>Hello World</h1> 
+
+// ReactDOM.render(
+//     App,
+//     document.getElementById('root')
+// )
 
 ReactDOM.render(
-    App,
+    // React.createElement(Component),
+    // Component(),
+    <Component />,
     document.getElementById('root')
-);
+)
+
+
