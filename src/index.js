b25e73820a85fa0024a211186e4413b8e0532ce1
Konrad <wudkowski@gmail.com>
2017-09-10 15:13:14 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// <h1>Hello World</h1>
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createElement('h1', null, 'Hello World');
+
+ReactDOM.render(
+    App,
+    document.getElementById('root')
+);
