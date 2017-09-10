afcf1acc3332e0fe18104e86a97179a97fbf854a
Konrad <wudkowski@gmail.com>
2017-09-10 19:24:22 +0100

    Shows app without props (hardcoded refs)

--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var createClass = require('create-react-class'); // npm i -S create-react-class
-
-var Component = createClass({
-    getInitialState: function() {
-        return {
-            color: "blue"
-        }
-    },
-    handleButtonClick: function() {
-        this.setState(function(prevState) {
-            return {
-                color: (prevState.color === "blue") ? "green" : "blue"
-            }
-        })
-    },
-    render : function() {
-        return (
-            <div>
-                <div style={{color: this.state.color}}>
-                    <h1> {this.props.greeting} </h1>  
-                </div>
-                <button onClick={this.handleButtonClick}>ClickMe</button>
-            </div>
-        )        
-    }
-})
+var Show = require('./Show')
 
 ReactDOM.render(
-    <Component greeting='hey there' />,
+    <Show />,
     document.getElementById('root')
 )
 
