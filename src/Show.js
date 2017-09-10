f34c88f5a151b7dfa09179af486c31eeb04242d4
Konrad <wudkowski@gmail.com>
2017-09-10 19:49:36 +0100

    Add dynamic loading of shows

--- src/Show.js
+++ src/Show.js
@@ -1,10 +1,11 @@
 var React = require('react')
 var createClass = require('create-react-class')
 
+
 var Title = createClass({
     render: function() {
         return (
-            <h3>Title: Friends</h3>
+            <h3>{this.props.showTitle}</h3>
         )
     }
 })
@@ -13,7 +14,7 @@ var Poster = createClass({
     render: function() {
         return(
             <img 
-                src="https://www.movieposter.com/posters/archive/main/52/MPW-26106"
+                src={this.props.showPoster}
                 alt="Show Poster"
                 style={{height: 400, width: 400}}
             />
@@ -26,21 +27,41 @@ var ShowInfo = createClass({
         return (
             <div>
                 <p>
-                    Follows the personal and professional lives of six 20 to 30-something-year-old friends living in Manhattan.
+                    {this.props.showPlot}
                 </p>
-                <h5>IMDB Rating: 9.0</h5>
+                <h5>IMDB Rating: {this.props.showRating}</h5>
             </div>
         )
     }
 })
 
 var Show = createClass({
+    getInitialState: function() {
+        return {
+            showIndex: this.props.showIndex
+        }
+    },
+    getDefaultProps: function() {
+        return {
+            showIndex: 0
+        }
+    },
+    handleButtonClick: function() {
+        var totalShows = this.props.shows.length;
+        this.setState(function(prevState) {
+            return {
+                showIndex: (prevState.showIndex + 1) % totalShows
+            };
+        })
+    },
     render: function() {
+    var show = this.props.shows[this.state.showIndex]        
         return (
             <div className="text-center" >
-                <Title />
-                <Poster />
-                <ShowInfo />
+                <Title showTitle={show.title} />
+                <Poster showPoster={show.poster} />
+                <ShowInfo showPlot={show.plot} showRating={show.imdbRating} />
+                <button onClick={this.handleButtonClick}>Next Show</button>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Show = require('./Show')
+var shows = require('./shows')
 
 ReactDOM.render(
-    <Show />,
+    <div>
+        <Show shows={shows} showIndex={1}/>
+    </div>,
     document.getElementById('root')
 )
 
